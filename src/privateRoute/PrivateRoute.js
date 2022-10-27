89051cb8247be9742eb66f5b53c3af017f265d07
minhaz000 <mrmminhaz@gamail.com>
2022-10-27 15:06:44 +0600

    site name chane and fav icon added

--- src/privateRoute/PrivateRoute.js
+++ src/privateRoute/PrivateRoute.js
@@ -6,7 +6,8 @@ import { AuthContext } from '../context/UserContext';
 function PrivateRoute({children}) {
   const {user, loading} = useContext(AuthContext)
   const location = useLocation();
-  if(loading===true){ return <div style={{minHeight:"80vh"}}> 
+  if(loading===true){ return <div className=' text-center mt-5' style={{minHeight:"80vh"}}> 
+  Loading ....
   <div class="spinner" > </div> </div>}
   if(user) {return children}
   else{return <Navigate to="/login" state={{ from : location  }}  replace  />}
